492df071dbe2b2603b53eea645e80b592c9889ba
BabyYoda741 <75110762+BabyYoda741@users.noreply.github.com>
2020-12-16 18:49:37 +0530

    Start

--- main.py
+++ main.py
@@ -1,27 +1,22 @@
 from tkinter import *
-from tkinter import ttk
-from random import *
 from tkinter import messagebox
-import os
+from tkinter import ttk
 
-
-
-
-class pycrypto():
-    def __init__(self,root):
-        self.root=root
+class Pycrypto():
+    def __init__(self, root):
+        self.root = root
         self.root.title("PyCrypto")
         self.root.geometry("1240x600+0+0")
-        abg="SteelBlue1"
+        abg = "SteelBlue1"
         self.root.configure(bg=abg)
-        bg_clr="DodgerBlue2"
-        t_clr="black"
-        bclr="DodgerBlue2"
-        title=Label(self.root,text="PyCrypto",bg=bg_clr,fg=t_clr,bd=12,relief=GROOVE,font=("arial", 30 ,"bold"))
+        bg_clr = "DodgerBlue2"
+        t_clr = "black"
+        bclr = "DodgerBlue2"
+        title = Label(self.root, text="PyCrypto", bg=bg_clr, fg=t_clr, bd=12, relief=GROOVE, font=("arial", 30, "bold"))
         title.pack(fill=X)
 
         self.text = StringVar()
-        self.algo = ""
+        self.algo = ''
         self.result = StringVar()
         self.key = IntVar()
 
@@ -30,20 +25,16 @@ class pycrypto():
         Label(F1, text="Enter text", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr).grid(row=0, column=0, padx=80, pady=20)
         Entry(F1, width=20, font="arial 15", bd=7, relief=SUNKEN,textvariable=self.text).grid(row=1, column=0, padx=10, pady=3)
 
-
         Label(self.root, text="Choose algorithm", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=20, pady=10).place(x=370, y=190)
         algochoosen = ttk.Combobox(self.root,values=[' Caesar Cipher',' Transposition Cipher',' Rail Fence Cipher'], font=("arial", 15, "bold"),width=22, textvariable=self.algo)
         algochoosen.place(x=350,y= 250)
         algochoosen.current(0)
 
-
         Label(self.root, text="Enter key", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=30, pady=10).place(x=370, y=300)
         Entry(self.root,width=20, font="arial 15",bd=7,textvariable=self.key,relief=SUNKEN).place(x=350, y=350)
 
-
         Button(self.root,width=15,text="Encrypt", font=("arial", 14, "bold"), bg=bclr, fg=t_clr, bd=7, command=self.encrypt).place(x=650, y=243 )
         Button(self.root, width=15,text="Decrypt",font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7).place(x=650,y=343)
-
 
         F2 = LabelFrame(self.root, text="Output", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr, bd=7,relief=SUNKEN)
         F2.place(x=890, y=100, width=320, height=370)
@@ -58,21 +49,17 @@ class pycrypto():
         clear_button.place(x=750, y=500)
 
     def encrypt(self):
-        a=str(self.text.get())
-        a = a.upper()
-        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        self.res=str(self.result.get())
-
+        self.result.set('')
+        a = str(self.text.get())
+        alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+        self.res = str(self.result.get())
         for letter in a:
             if letter in alpha:
                 letter_index = (alpha.find(letter) + int(self.key.get())) % len(alpha)
                 self.res = self.res + str(alpha[letter_index])
             else:
                 self.res = self.res + str(letter)
-
         self.result.set(self.res)
-
-
 
     def clear_all(self):
         op = messagebox.askyesno("clear", "Do you really want to clear all data?")
@@ -86,7 +73,7 @@ class pycrypto():
         if op > 0:
             self.root.destroy()
 
-if __name__=="__main__":
-    root=Tk()
-    pycrypto(root)
+if __name__ == "__main__":
+    root = Tk()
+    Pycrypto(root)
     root.mainloop()
