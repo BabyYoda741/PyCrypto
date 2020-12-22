258c68c2051692dca7fb1a38f8577f9a9b9cc11d
BabyYoda741 <75110762+BabyYoda741@users.noreply.github.com>
2020-12-23 01:37:46 +0530

    beta2

--- main.py
+++ main.py
@@ -6,13 +6,13 @@ class Pycrypto():
     def __init__(self, root):
         self.root = root
         self.root.title("PyCrypto")
-        self.root.geometry("1240x600+0+0")
-        abg = "SteelBlue1"
+        self.root.geometry("1200x600+0+0")
+        abg = "Cyan3"
         self.root.configure(bg=abg)
-        bg_clr = "DodgerBlue2"
+        bg_clr = "Cyan4"
         t_clr = "black"
-        bclr = "DodgerBlue2"
-        title = Label(self.root, text="PyCrypto", bg=bg_clr, fg=t_clr, bd=12, relief=GROOVE, font=("arial", 30, "bold"))
+        bclr = "Orange"
+        title = Label(self.root, text="PyCrypto", bg=bg_clr, fg=t_clr, bd=12, relief=GROOVE, font=("arial", 30))
         title.pack(fill=X)
 
         self.text = StringVar()
@@ -20,34 +20,28 @@ class Pycrypto():
         self.result = StringVar()
         self.key = IntVar()
 
-        F1 = LabelFrame(self.root, text="Input", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr, bd=7,relief=SUNKEN)
-        F1.place(x=5, y=100, width=320, height=370)
-        Label(F1, text="Enter text", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr).grid(row=0, column=0, padx=80, pady=20)
-        Entry(F1, width=20, font="arial 15", bd=7, relief=SUNKEN,textvariable=self.text).grid(row=1, column=0, padx=10, pady=3)
+        Label(self.root, text="Input Text", font=("arial", 15, "bold"), bg=abg, fg=t_clr).place(x=50, y=100)
+        Entry(self.root, width=100, font="arial 15", bd=7, relief=SUNKEN,textvariable=self.text).place(x=50, y=170)
 
-        Label(self.root, text="Choose algorithm", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=20, pady=10).place(x=370, y=190)
-        algochoosen = ttk.Combobox(self.root,values=['Caesar Cipher','ROT13 Cipher','Transposition Cipher'], font=("arial", 15, "bold"),width=22, textvariable=self.algo)
-        algochoosen.place(x=350,y= 250)
+        Label(self.root, text="Choose Algorithm", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=20, pady=10).place(x=100, y=250)
+        algochoosen = ttk.Combobox(self.root,values=['Caesar Cipher','ROT13 Cipher','Transposition Cipher'], font=("arial", 13),width=22, textvariable=self.algo)
+        algochoosen.place(x=300,y= 260)
         algochoosen.current(0)
 
-        Label(self.root, text="Enter key", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=30, pady=10).place(x=370, y=300)
-        Entry(self.root,width=20, font="arial 15",bd=7,textvariable=self.key,relief=SUNKEN).place(x=350, y=350)
+        Label(self.root, text="Enter Key", font=("arial", 15, "bold"), bg=abg, fg=t_clr, padx=30, pady=10).place(x=600, y=250)
+        Entry(self.root,width=20, font="arial 15",bd=7,textvariable=self.key,relief=SUNKEN,).place(x=730, y=255)
 
-        Button(self.root,width=15,text="Encrypt", font=("arial", 14, "bold"), bg=bclr, fg=t_clr, bd=7, command=self.encrypt).place(x=650, y=243 )
-        Button(self.root, width=15,text="Decrypt",font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7, command=self.decrypt).place(x=650,y=343)
-        Button(self.root, width=15,text="Swap Output",font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7, command=self.swap).place(x=550,y=500)
+        Button(self.root,width=15,text="Encrypt", font=("arial", 14, "bold"), bg=bclr, fg=t_clr, bd=7, command=self.encrypt).place(x=70, y=490 )
+        Button(self.root, width=15,text="Decrypt",font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7, command=self.decrypt).place(x=290,y=490)
+        Button(self.root, width=15,text="Swap Output",font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7, command=self.swap).place(x=510,y=490)
 
-        F2 = LabelFrame(self.root, text="Output", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr, bd=7,relief=SUNKEN)
-        F2.place(x=890, y=100, width=320, height=370)
+        Label(self.root, text="Output Text", font=("arial", 15, "bold"), bg=abg, fg=t_clr).place(x=50, y=330)
+        Entry(self.root, width=100, font="arial 15", bd=7,textvariable=self.result,  relief=SUNKEN).place(x=50, y=400)
 
-        Label(F2, text="Result", font=("arial", 15, "bold"), bg=bg_clr, fg=t_clr).grid(row=0, column=0, padx=80, pady=20)
-        Entry(F2, width=20, font="arial 15", bd=7,textvariable=self.result,  relief=SUNKEN).grid(row=1, column=0, padx=40, pady=3)
-
-        exit_button = Button(self.root, width=15,text="Exit",command = self.exit ,font=("arial", 14, "bold"), bg="DodgerBlue2", fg=t_clr,bd=7)
-        exit_button.place(x=950, y=500)
-
-        clear_button = Button(self.root, width=15, text="Clear",command= self.clear_all ,font=("arial", 14, "bold"), bg="DodgerBlue2",fg=t_clr, bd=7)
-        clear_button.place(x=750, y=500)
+        exit_button = Button(self.root, width=15,text="Exit",command = self.exit ,font=("arial", 14, "bold"), bg=bclr, fg=t_clr,bd=7)
+        exit_button.place(x=950, y=490)
+        clear_button = Button(self.root, width=15, text="Clear",command= self.clear_all ,font=("arial", 14, "bold"), bg=bclr,fg=t_clr, bd=7)
+        clear_button.place(x=730, y=490)
 
     def encrypt(self):
         self.result.set('')
@@ -70,6 +64,8 @@ class Pycrypto():
                 else:
                     self.res = self.res + str(letter)
             self.result.set(self.res)
+        if str(self.text.get()) == '':
+            messagebox.showerror("Error", "Input text to encrypt")
 
     def decrypt(self):
         self.result.set('')
@@ -92,13 +88,18 @@ class Pycrypto():
                 else:
                     self.res = self.res + str(letter)
             self.result.set(self.res)
+        if str(self.text.get()) == '':
+            messagebox.showerror("Error", "Input text to Decrypt")
 
     def swap(self):
-        self.text.set(str(self.result.get()))
-        self.result.set('')
+        if str(self.result.get()) == '':
+            messagebox.showerror("Error","Output is empty")
+        else:
+            self.text.set(str(self.result.get()))
+            self.result.set('')
 
     def clear_all(self):
-        op = messagebox.askyesno("clear", "Do you really want to clear all data?")
+        op = messagebox.askyesno("Clear", "Do you really want to clear all data?")
         if op > 0:
             self.text.set("")
             self.result.set("")
